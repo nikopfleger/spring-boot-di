99f8c4b2d542688fa9c2c90eae96a13506913087
Nicolas Pfleger <nicolas.pfleger@gmail.com>
2021-04-09 15:21:24 -0300

    Clase 37

--- src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -1,16 +1,41 @@
 package com.bolsadeideas.springboot.di.app.controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
+import com.bolsadeideas.springboot.di.app.models.service.IServicio;
+
 
 @Controller
 public class IndexController {
-
-	private MiServicio servicio = new MiServicio();
 	
+	//lo inyectamos con autowired, solo significa eso, busca el tipo MiServicio y lo inyecta
+	//para desacoplar totalmente private MiServicio servicio; tengo que usar una interfaz generica
+	//asi si algun dia cambio el tipo de objeto/clase no depende de mi codigo y queda encapsulado.
+	//LO hago desde Interfaz
+	
+	
+	//Si tengo mas de una clase que usa IServicio de interfaz tengo que especificar sino uso @Autowired
+	//Con @Qualifier le digo cual quiero (por ejemplo si no es el primario), puedo usar Qualifier sin Primary
+	//Conviene cambiar los nombres en las clases para no tocar esta parte y pueda ser transparente
+	@Autowired
+	@Qualifier("miServicioPrincipal")
+	private IServicio servicio;
+	
+	/*
+	//Cuando lo hago por constructor puedo omitir el autowired, sino si uso el setter le tengo que poner
+	@Autowired
+	public IndexController(IServicio servicio) {
+
+		this.servicio = servicio;
+	}*/
+
+
+
 	@GetMapping({"/","","/index"})
 	public String index(Model model) {
 		
@@ -18,4 +43,7 @@ public class IndexController {
 		
 		return "index";
 	}
+
+
+	
 }
--- src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicioComplejo.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicioComplejo.java
@@ -1,20 +1,19 @@
 package com.bolsadeideas.springboot.di.app.models.service;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 //@Component hace que sea una componente de spring y podamos inyectarlo para desacoplar
 //Puedo usar @Service, es lo mismo solamente indica que es una fachada de logica de negocio
-//Primary le digo la que quiero que sea por defecto
-@Component("miServicioSimple")
-@Primary
-public class MiServicio implements IServicio {
+
+@Component("miServicioPrincipal")
+
+public class MiServicioComplejo implements IServicio {
 	
 	
 	//NOTA: SI TENEMOS CONSTRUCTOR TENEMOS QUE ADEMAS TENER UN CONSTRUCTOR VACIO, YA QUE SPRING LOS MANEJA AUTOMATICAMENTE CON EL CONSTRUCTOR SIN PARAMETROS
 
 	@Override
 	public String operacion() {
-		return "ejecutando algún proceso importante simple...";
+		return "ejecutando algún proceso importante complicado...";
 	}
 }
